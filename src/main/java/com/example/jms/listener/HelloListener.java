8643d2d42a9551644de7b84bc7fa63d4879eba36
Paco Muñoz <iicc96@hotmail.com>
2021-12-25 20:13:53 +0100

    hello world listener

---
+++ src/main/java/com/example/jms/listener/HelloListener.java
@@ -0,0 +1,21 @@
+package com.example.jms.listener;
+
+import com.example.jms.config.JmsConfig;
+import com.example.jms.model.HelloWorldMessage;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HelloListener {
+
+    @JmsListener(destination = JmsConfig.QUEUE)
+    public void listen(@Payload HelloWorldMessage helloWorldMessage, @Headers MessageHeaders headers, Message message) {
+        System.out.println("I have a message!!!");
+        System.out.println(helloWorldMessage);
+    }
+
+}
--- src/main/java/com/example/jms/sender/HelloSender.java
+++ src/main/java/com/example/jms/sender/HelloSender.java
@@ -1,13 +1,30 @@
 package com.example.jms.sender;
 
+import com.example.jms.config.JmsConfig;
+import com.example.jms.model.HelloWorldMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.UUID;
+
+@RequiredArgsConstructor
 @Component
 public class HelloSender {
 
+    private final JmsTemplate jmsTemplate;
+
     @Scheduled(fixedRate = 2000)
     public void sendMessage() {
+
+        HelloWorldMessage message = HelloWorldMessage.builder()
+                .id(UUID.randomUUID())
+                .message("Hola Mundo!!!")
+                .build();
+
+        System.out.println("Enviando mensaje...");
+        jmsTemplate.convertAndSend(JmsConfig.QUEUE, message);
         System.out.println("Mensaje enviado!");
     }
 
